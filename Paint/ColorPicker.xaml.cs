1c7e50b64ba2b1a4abfbcebabe16c9b1c070256a
LukasNowy <nowy492l@htl-ottakring.ac.at>
2017-02-03 23:48:52 +0100

    Second Commit

--- Paint/ColorPicker.xaml.cs
+++ Paint/ColorPicker.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Paint
 {
     /// <summary>
-    /// Interaktionslogik für MainWindow.xaml
+    /// Interaktionslogik für ColorPicker.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ColorPicker : Window
     {
-        public MainWindow()
+        public ColorPicker()
         {
             InitializeComponent();
-            INIT();
-        }
-
-        public void INIT()
-        {
-
         }
     }
 }
